070243552c4e3620164ea6d9162373bba849fa7a
freezy fritz <Akuedesmondfav1234@gmail.com>
2019-11-28 13:42:09 +0100

    my shopping cart with javascript function

---
+++ js/shopping.js
@@ -0,0 +1,22 @@
+
+let changeButtonTxt = document.getElementById("changeTxt")
+let viewShoppingCart = document.getElementById("viewShoppingCart");
+let selectMoreItems = document.getElementById("selectShoppingCart");
+let showingSelectMoreItemsElement= false;
+
+selectMoreItems.style.display= "none";
+viewShoppingCart.style.display= "block";
+
+function changeView(){
+    if (showingSelectMoreItemsElement===true){
+        selectMoreItems.style.display= "none";
+        viewShoppingCart.style.display= "block";
+        showingSelectMoreItemsElement= false;
+        changeButtonTxt.innerHTML = "view shopping cart" 
+    }else{
+        changeButtonTxt.innerHTML = "select more items" 
+        selectMoreItems.style.display = "block";
+        viewShoppingCart.style.display= "none";
+        showingSelectMoreItemsElement= true;
+    }
+}
